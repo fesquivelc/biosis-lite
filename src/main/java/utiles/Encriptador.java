4625296f54c236f06855d35d41825fc2ffb7bc2b
fesquivelc <fesquivelc@localhost>
2015-01-02 13:58:58 -0500

    CONFIGURACION DE BASES DE DATOS CON PASSWORD

--- src/main/java/utiles/Encriptador.java
+++ src/main/java/utiles/Encriptador.java
@@ -41,81 +41,32 @@ public class Encriptador {
         try {
             String passPhrase = "3lh4ck3r12";
             KeySpec keySpec = new PBEKeySpec(passPhrase.toCharArray(), salt, iterationCount);
-            
+
             // create a secret (symmetric) key using PBE with MD5 and DES
             SecretKey key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
-            
+
             // construct a parameter set for password-based encryption as defined in the PKCS #5 standard
             AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterationCount);
-            
+
             ecipher = Cipher.getInstance(key.getAlgorithm());
             dcipher = Cipher.getInstance(key.getAlgorithm());
-            
+
             // initialize the ciphers with the given key
             ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
-            
+
             dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
         } catch (NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException ex) {
             Logger.getLogger(Encriptador.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-//    public static void main(String[] args) {
-//
-//        try {
-//
-//            String passPhrase = "My Secret Password";
-//
-//            // create a user-chosen password that can be used with password-based encryption (PBE)
-//            // provide password, salt, iteration count for generating PBEKey of fixed-key-size PBE ciphers
-//            KeySpec keySpec = new PBEKeySpec(passPhrase.toCharArray(), salt, iterationCount);
-//
-//            // create a secret (symmetric) key using PBE with MD5 and DES 
-//            SecretKey key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
-//
-//            // construct a parameter set for password-based encryption as defined in the PKCS #5 standard
-//            AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterationCount);
-//
-//            ecipher = Cipher.getInstance(key.getAlgorithm());
-//            dcipher = Cipher.getInstance(key.getAlgorithm());
-//
-//            // initialize the ciphers with the given key
-//            ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
-//
-//            dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
-//
-//            String encrypted = encrypt("This is a classified message!");
-//
-//            String decrypted = decrypt(encrypted);
-//
-//            System.out.println("Decrypted: " + decrypted);
-//
-//        } catch (InvalidAlgorithmParameterException e) {
-//            System.out.println("Invalid Alogorithm Parameter:" + e.getMessage());
-//            return;
-//        } catch (InvalidKeySpecException e) {
-//            System.out.println("Invalid Key Spec:" + e.getMessage());
-//            return;
-//        } catch (NoSuchAlgorithmException e) {
-//            System.out.println("No Such Algorithm:" + e.getMessage());
-//            return;
-//        } catch (NoSuchPaddingException e) {
-//            System.out.println("No Such Padding:" + e.getMessage());
-//            return;
-//        } catch (InvalidKeyException e) {
-//            System.out.println("Invalid Key:" + e.getMessage());
-//            return;
-//        }
-//
-//    }
-
     public static String encrypt(String str) {
 
         try {
-            if(ecipher == null){
+            if (ecipher == null) {
                 iniciar();
             }
-  	// encode the string into a sequence of bytes using the named charset
+            // encode the string into a sequence of bytes using the named charset
             // storing the result into a new byte array. 
             byte[] utf8 = str.getBytes("UTF8");
 
@@ -139,7 +90,7 @@ public class Encriptador {
     public static String decrypt(String str) {
 
         try {
-            if(dcipher == null){
+            if (dcipher == null) {
                 iniciar();
             }
             // decode with base64 to get bytes
